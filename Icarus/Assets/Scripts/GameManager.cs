6de453e62a0ef5df95714cb7e9a3884589894d81
BRE15150326 <C-brewer@hotmail.co.uk>
2018-11-18 21:03:40 +0000

    UI and slight animation
    Made the starting UI and started applying animation to the player

---
+++ Icarus/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+    public void EndGame()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
---
+++ Icarus/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float Speed = 3;
+    private Vector2 moveVelocity;
+    private Rigidbody2D rb;
+    private bool MovingUp = false;
+    private bool MovingRight = false;
+    private bool MoveDown = false;
+    private bool MoveLeft = false;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * Speed;
+	}
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
