de25146fa51c888b4a80e851a199f73230714301
BRE15150326 <C-brewer@hotmail.co.uk>
2018-11-18 22:25:40 +0000

    Player Movement Animations
    When the player moves, animations are now played depending on what direction it is moving, and Idle animations have been applied

--- Icarus/Assets/Scripts/PlayerMovement.cs
+++ Icarus/Assets/Scripts/PlayerMovement.cs
@@ -4,27 +4,69 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    // used to apply multiple animations
+    public Animator anim;
+    // Contorlling the speed of player
     public float Speed = 3;
     private Vector2 moveVelocity;
     private Rigidbody2D rb;
-    private bool MovingUp = false;
-    private bool MovingRight = false;
-    private bool MoveDown = false;
-    private bool MoveLeft = false;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        // Player movement
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         moveVelocity = moveInput.normalized * Speed;
+
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        anim.SetFloat("SpeedX", inputX);
+        anim.SetFloat("SpeedY", inputY);
 	}
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        float lastInputX = Input.GetAxis("Horizontal");
+        float lastInputY = Input.GetAxis("Vertical");
+
+        // Checking if the player is moving to call the animation and set it to true
+        if (lastInputX != 0 || lastInputY != 0)
+        {
+            anim.SetBool("Moving", true);
+            if (lastInputX > 0)
+            {
+                anim.SetFloat("LastMoveX", 1f);
+            } else if (lastInputX < 0)
+            {
+                anim.SetFloat("LastMoveX", -1f);
+            } else
+            {
+                anim.SetFloat("LastMoveX", 0f);
+            }
+
+            if (lastInputY > 0)
+            {
+                anim.SetFloat("LastMoveY", 1f);
+            } else if (lastInputY < 0)
+            {
+                anim.SetFloat("LastMoveY", -1f);
+            } else
+            {
+                anim.SetFloat("LastMoveY", 0f);
+            }
+        }
+        else
+        {
+            anim.SetBool("Moving", false);
+        }
     }
 }
