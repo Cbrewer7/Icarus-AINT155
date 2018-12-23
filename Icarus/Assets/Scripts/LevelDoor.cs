bcdee2e35d6520a4006425afeadab77151a78b1d
BRE15150326 <C-brewer@hotmail.co.uk>
2018-12-23 18:06:38 +0000

    Door to next level
    A game object that takes the player to the next level when it's collided with. (Not Textured)

---
+++ Icarus/Assets/Scripts/LevelDoor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelDoor : MonoBehaviour {
+
+
+    // Allows the ability to choose the next level in the inspector
+    // rather than have mulitple scripts for the same function
+    [SerializeField] private string newLevel;
+
+
+    // When the player collides with this object a new level will load
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(newLevel);
+        }
+    }
+}
