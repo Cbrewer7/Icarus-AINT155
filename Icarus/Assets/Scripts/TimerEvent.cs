7ffd33679150963c4b5191e469ed359bda88ed76
BRE15150326 <C-brewer@hotmail.co.uk>
2018-12-30 03:18:23 +0000

    Added Assets
    More tiles added to the tilemap to make a more natural looking background.

--- Icarus/Assets/Scripts/TimerEvent.cs
+++ Icarus/Assets/Scripts/TimerEvent.cs
@@ -5,6 +5,8 @@ using UnityEngine.Events;
 
 public class TimerEvent : MonoBehaviour {
 
+    // TODO - Assign the amount of times the spawner will spawn
+    public float amountofspawns = 5;
     public float time = 1;
     public bool repeat = false;
     public UnityEvent onTimerComplete;
