a032754a3096352cded81aeb26a41461c20d5bf5
BRE15150326 <C-brewer@hotmail.co.uk>
2019-01-12 18:46:46 +0000

    Level Changing bug fixes
    Added new level "Level01(Tutcontinue)" level to replace Level01 when the player wants to use the tutorial instead of going straight into the game.
    Fixed Spawning into new levels. Now the player starts in the correct position when loading into a new level.

--- Icarus/Assets/Scripts/PlayerSpawn.cs
+++ Icarus/Assets/Scripts/PlayerSpawn.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class PlayerSpawn : MonoBehaviour {
 
+    private Playersystem thePlayer;
+    private CameraFollow theCamera;
+
 	// Use this for initialization
 	void Start () {
-		
+        // Will find the game object with the Playersystem scrip attached
+        // (the Player) and will move them onto the starting point
+        thePlayer = FindObjectOfType<Playersystem>();
+        thePlayer.transform.position = transform.position;
+
+        // Ensuring the Camera also starts at the start point with the Player object
+        theCamera = FindObjectOfType<CameraFollow>();
+        theCamera.transform.position = new Vector3(transform.position.x, transform.position.y, theCamera.transform.position.z);
 	}
 	
 	// Update is called once per frame
