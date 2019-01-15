997406e262a195bf2d9ca60ac8af7a0fc04c4a18
BRE15150326 <C-brewer@hotmail.co.uk>
2019-01-15 00:28:28 +0000

    Finished Levels + Tutorial
    The original 10 levels have been completed as well as the tutorial which introduces the player to the controls and world.

---
+++ Icarus/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public GameObject dialogueBox;
+    public GameObject controlsScroll;
+    public Text dialogueText;
+
+    public bool dialogueActive;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (dialogueActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            controlsScroll.SetActive(false);
+            dialogueBox.SetActive(false);
+            dialogueActive = false;
+        }
+	}
+}
